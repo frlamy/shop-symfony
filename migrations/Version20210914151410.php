97b04a74281e8a4cbd58ec5d58b43ffd5ff289ff
Franck L <nijigatatsu@gmail.com>
2021-09-27 13:42:03 +0200

    Bingo

--- migrations/Version20210914151410.php
+++ migrations/Version20210914151410.php
@@ -20,12 +20,6 @@ final class Version20210914151410 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE full_name full_name VARCHAR(255) NOT NULL');
-    }
-
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE `user` (
             `id` int(11) NOT NULL AUTO_INCREMENT,
             `email` varchar(180) COLLATE utf8mb4 NOT NULL,
@@ -35,5 +29,11 @@ final class Version20210914151410 extends AbstractMigration
             PRIMARY KEY (`id`),
             UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
            ) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
     }
 }
